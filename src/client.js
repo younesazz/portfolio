f1106e66e2715f25352b065c224a2bba8cda7d9c
Hamani_Younes <68382115+younesazz@users.noreply.github.com>
2022-10-02 03:32:00 +0100

    Add files via upload

---
+++ src/client.js
@@ -0,0 +1,15 @@
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = sanityClient({
+  projectId: 'tyd3ua8o',
+  dataset: 'production',
+  apiVersion: '2022-11-01',
+  useCdn: false,
+  ignoreBrowserTokenWarning: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
